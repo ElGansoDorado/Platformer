367cc570dede5aa589bf0ced295fd36d1c788bcb
ElGansoDorado <yakovchikdenis@gmail.com>
2023-10-01 17:39:55 +0300

    feat: added jump and run

--- Assets/Scriptes/Player.cs
+++ Assets/Scriptes/Player.cs
@@ -1,26 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float speed;
-    public float jumpHeight;
+    SpriteRenderer sr;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
 
     void FixedUpdate() 
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (Input.GetButton("Jump"))
+        {
+            Jump();
+        }
     }
 
     void Update()
     {
-        
+        Flip();   
+    }
+
+    private void Jump() => rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+
+    private void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = false;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+        }
     }
 }
