b63538677667073bfe47a7c9b2ff1bc9e1560c00
Денис Яковчик <87077649+ElGansoDorado@users.noreply.github.com>
2023-10-08 23:46:15 +0300

    TASK-4: First enemy (#3)
    * fix wall
    * added saw
    * added damage
    * added death

---
+++ Assets/Scriptes/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+           other.gameObject.GetComponent<Player>().RecountHp(-1);
+           other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 8f, ForceMode2D.Impulse);
+        }
+    }
+}
--- Assets/Scriptes/Player.cs
+++ Assets/Scriptes/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Common;
 using UnityEditor.Tilemaps;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -20,8 +21,13 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private Main main;
     [SerializeField] private Transform groundCheck;
     private bool isGround;
+
+    private int curHp = 3;
+    private int maxHp = 3;
+    private bool isHit = false;
 
     void Start()
     {
@@ -86,5 +92,55 @@ public class Player : MonoBehaviour
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
         isGround = colliders.Length > 1;
+    }
+
+    public void RecountHp(int deltaHp)
+    {
+        curHp = curHp + deltaHp;
+
+        if (deltaHp < 0)
+        {
+            StopCoroutine(OnHit());
+
+            isHit = true;
+
+            StartCoroutine(OnHit());
+        }
+
+        if (curHp <= 0)
+        {
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            Invoke("Lose", 1.5f);
+        }
+    }
+
+    IEnumerator OnHit()
+    {
+        if (isHit)
+        {
+            sr.color = new Color(1f, sr.color.g - 0.04f, sr.color.b - 0.04f);
+        }
+        else
+        {
+            sr.color = new Color(1f, sr.color.g + 0.04f, sr.color.b + 0.04f);
+        }
+
+        if (sr.color.g == 1f)
+        {
+            StopCoroutine(OnHit());
+        }
+
+        if (sr.color.g <= 0)
+        {
+            isHit = false;
+        }
+
+        yield return new WaitForSeconds(0.02f);
+        StartCoroutine(OnHit());
+    }
+
+    private void Lose()
+    {
+        main.GetComponent<Main>().Lose();
     }
 }
