631aae413ae898acebe69ec6923dccabaaf849fa
ElGansoDorado <yakovchikdenis@gmail.com>
2023-10-15 22:02:06 +0300

    TASK-6: Air enemies points update

--- Assets/Scriptes/EnemyAirPatrol.cs
+++ Assets/Scriptes/EnemyAirPatrol.cs
@@ -5,40 +5,47 @@ using UnityEngine;
 public class EnemyAirPatrol : Enemy
 {
     [SerializeField] private float speed = 1.5f;
-    [SerializeField] private Transform point1;
-    [SerializeField] private Transform point2;
+    [SerializeField] private Transform[] points;
+    private byte i = 0;
 
     private float waitTime = 2f;
     private bool canGo = true;
 
     void Start()
     {
-        gameObject.transform.position = new Vector3(point1.position.x, transform.position.y, transform.position.z);
+        gameObject.transform.position = new Vector3(points[i].position.x, transform.position.y, transform.position.z);
     }
 
     void Update()
     {
         if (canGo)
         {
-            transform.position = Vector3.MoveTowards(transform.position, point1.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
         } 
 
-        if (transform.position == point1.position)
+        if (transform.position == points[i].position)
         {
-            Transform point = point1;
-            point1 = point2;
-            point2 = point;
+            if (i < points.Length - 1)
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
 
             canGo = false;
             StartCoroutine(Waiting());
         }
     }
 
-    IEnumerator Waiting()
+    private IEnumerator Waiting()
     {
         yield return new WaitForSeconds(waitTime);
 
-        transform.eulerAngles = (transform.rotation.y == 0) ? new Vector3(0, 180, 0) : new Vector3(0,0,0);
+        byte point = (i - 1 > -1) ? (byte) (i - 1) : (byte) (points.Length - 1); 
+
+        transform.eulerAngles = (points[point].transform.position.x < points[i].transform.position.x) ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
 
         canGo = true;   
     }
