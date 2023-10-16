5ce7a6ede6922159436314bdb33ac49a98dbfdbf
ElGansoDorado <yakovchikdenis@gmail.com>
2023-10-16 23:17:43 +0300

    TASK-6: added death for enemy

--- Assets/Scriptes/EnemyPatrol.cs
+++ Assets/Scriptes/EnemyPatrol.cs
@@ -5,9 +5,20 @@ public class EnemyPatrol : Enemy
     [SerializeField] private float speed = 1.5f;
     [SerializeField] private bool moveLeft = true;
     [SerializeField] private Transform groundDetect;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        if (anim.GetBool("dead")) 
+        {
+            return;
+        }
+
         transform.Translate(Vector2.left * speed * Time.deltaTime);
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetect.position, Vector2.down, 1f);
 
